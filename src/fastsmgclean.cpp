2673a6be4e02055e5530a3e9b53cce709b5ef551
Qi Zheng <zhengqi@upenn.edu>
2017-11-22 16:20:10 -0500

    Autotool based project initiated

---
+++ src/fastsmgclean.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : dmfastclean.cpp
+// Author      : Qi Zheng
+// Version     :
+// Copyright   : GPL v3.0 Copyright (C) 2017  Qi Zheng
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
