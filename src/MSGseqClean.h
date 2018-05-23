70bf7d89dd86d2e42ea16fda0e4d655ffedbb377
Qi Zheng <zhengqi@upenn.edu>
2018-05-23 12:01:07 -0400

    program level include file added

---
+++ src/MSGseqClean.h
@@ -0,0 +1,19 @@
+/*
+ * MSGseqClean.h
+ *
+ *  Created on: May 23, 2018
+ *      Author: zhengqi
+ */
+
+#ifndef SRC_MSGSEQCLEAN_H_
+#define SRC_MSGSEQCLEAN_H_
+
+#include "DNAalphabet.h"
+#include "DNAseq.h"
+#include "PrimarySeq.h"
+#include "SeqIO.h"
+#include "RFMIndex.h"
+#include "MetaGenome.h"
+#include "MSGseqCleanConst.h"
+
+#endif /* SRC_MSGSEQCLEAN_H_ */
