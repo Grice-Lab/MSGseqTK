4868f39be439982f27d2fab0ba512703c23c31bf
Qi Zheng <e00011027@gmail.com>
2018-08-16 13:17:30 -0400

    Copyright information added

--- src/msgseqtk-mergedb.cpp
+++ src/msgseqtk-mergedb.cpp
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * This file is part of MSGseqTK, a Metagenomics Shot-Gun sequencing ToolKit
+ * for ultra-fast and accurate MSG-seq cleaning, mapping and more,
+ * based on space-efficient FM-index on entire collection of meta-genomics sequences.
+ * Copyright (C) 2018  Qi Zheng
+ *
+ * MSGseqTK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MSGseqTK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AlignerBoost.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 /*
  * msgseqtk-merge.cpp
  *
