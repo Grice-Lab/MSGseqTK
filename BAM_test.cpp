930eca47ce7f3371189d5b00ec0db383d71a8fc9
Qi Zheng <e00011027@gmail.com>
2018-11-17 12:55:37 -0500

    Introducing SAMtools classesa, with new C++11 and htslib dependency

---
+++ BAM_test.cpp
@@ -0,0 +1,14 @@
+/*
+ * BAM_test.cpp
+ *
+ *  Created on: Nov 14, 2018
+ *      Author: zhengqi
+ */
+
+#include "../src/BAM.h"
+
+namespace EGriceLab {
+namespace MSGseqTK {
+
+} /* namespace MSGseqTK */
+} /* namespace EGriceLab */
